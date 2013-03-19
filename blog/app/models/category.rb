fb1f7afedb0fde4d5d41db64716769568eaef935
Carlos García-Ramos <caco85@gmail.com>
2013-03-19 10:40:13 +0000

    chapter 7 done

---
+++ blog/app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ActiveRecord::Base
+  attr_accessible :name
+
+  has_and_belongs_to_many :articles
+
+  default_scope order('categories.name')
+end
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9640ea9230a7fdf69caf2bf47a37ffe013a096079c5a15cc98f806f55b45596e07dd74ff1880dccef859f2e2b4a1708d51cb1b604f2531cf3d8b587afe9f04bd'
